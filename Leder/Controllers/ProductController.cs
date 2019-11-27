d256a8e0e09fec76bbc57331d7d1daf074b58d26
Noelle Hsu <ohyanana2@gmail.com>
2019-11-27 10:02:13 +0800

    用PartialView精簡Product的View，還有放大鏡功能要處理

--- Leder/Controllers/ProductController.cs
+++ Leder/Controllers/ProductController.cs
@@ -9,58 +9,94 @@ namespace Leder.Controllers
 {
     public class ProductController : Controller
     {
-        List<Product> products = new List<Product>
+        //側背包
+        List<Product> totebags = new List<Product>
+            {      
+                new Product{ Id =1, Name = "Clarte 流蘇迷你箱型包", Category="Totebag", Photo="Clarte 流蘇迷你箱型包.jpg"},
+                new Product{ Id =2, Name = "Diario 迷你隨行斜肩袋", Category="Totebag", Photo="Diario 迷你隨行斜肩袋.jpg"},
+                new Product{ Id =3, Name = "Plota 防水兩用斜背包", Category="Totebag", Photo="Plota 防水兩用斜背包.jpg"},
+                new Product{ Id =4, Name = "Tone Oilnume 兩用托特包", Category="Totebag", Photo="Tone Oilnume 兩用托特包.jpg"},
+                new Product{ Id =5, Name = "Tone Oilnume 拉鍊斜背包", Category="Totebag", Photo="Tone Oilnume 拉鍊斜背包.jpg"},
+                new Product{ Id =6, Name = "Tone Oilnume 迷你郵差包", Category="Totebag", Photo="Tone Oilnume 迷你郵差包.jpg"},
+            };
+
+        //後背包
+        List<Product> backpacks = new List<Product>
             {
-                new Product{ Name = "Backpack001", Category="Backpack", Photo="Backpack.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "Backpack002", Category="Backpack", Photo="Backpack2.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "CoinWallet001", Category="CoinWallet", Photo="CoinWallet.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "CoinWallet002", Category="CoinWallet", Photo="CoinWallet2.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "Longclip001", Category="Longclip", Photo="Longclip.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "Longclip002", Category="Longclip", Photo="Longclip2.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "NameCard001", Category="NameCard", Photo="NameCard.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "NameCard002", Category="NameCard", Photo="NameCard2.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "Totebag001", Category="Totebag", Photo="Totebag.jpg", ProductPage="https://shorturl.at/aiOS0" },
-                new Product{ Name = "Totebag002", Category="Totebag", Photo="Totebag2.jpg", ProductPage="https://shorturl.at/aiOS0" },
+                new Product{ Id =1, Name = "Tone Oilnume 中型後背包", Category="Backpack", Photo="Tone Oilnume 中型後背包.jpg"},
+                new Product{ Id =2, Name = "Tone Oilnume 後背包", Category="Backpack", Photo="Tone Oilnume 後背包.jpg"},
 
             };
 
-        //把Model帶入，傳給View
-        public ActionResult Product()
-        {
-            return View(products);
-        }
+        //長夾
+        List<Product> longclips = new List<Product>
+            {
+                new Product{ Id =1, Name = "Belchord 長夾", Category="Longclip", Photo="Belchord 長夾.jpg"},
+                new Product{ Id =2, Name = "Diario 長夾", Category="Longclip", Photo="Diario 長夾.jpg"},
+                new Product{ Id =3, Name = "Tone Oilnume 蛙嘴式零錢長夾", Category="Longclip", Photo="Tone Oilnume 蛙嘴式零錢長夾.jpg"},
+                new Product{ Id =4, Name = "Tone Oilnume 纏繞式長夾", Category="Longclip", Photo="Tone Oilnume 纏繞式長夾.jpg"},
+                new Product{ Id =5,  Name = "Urbano 長皮夾", Category="Longclip", Photo="Urbano 長皮夾.jpg"},
 
-        //預設是顯示側背包類別
-        public ActionResult ProductMainPage()
+        };
+
+        //零錢包
+        List<Product> coinwallets = new List<Product>
+            {
+                new Product{ Id =1, Name = "Belchord零錢包", Category="Coinwallet", Photo="Belchord零錢包.jpg"},
+                new Product{ Id =2, Name = "Bridle方形零錢包", Category="Coinwallet", Photo="Bridle方形零錢包.jpg"},
+                new Product{ Id =3, Name = "Diario 雙釦式零錢包", Category="Coinwallet", Photo="Diario 雙釦式零錢包.jpg"},
+                new Product{ Id =4, Name = "Tone Oilnume 蛙嘴式迷你零錢包", Category="Coinwallet", Photo="Tone Oilnume 蛙嘴式迷你零錢包.jpg"},
+                new Product{ Id =5, Name = "Tone Oilnume 蛙嘴式零錢袋", Category="Coinwallet", Photo="Tone Oilnume 蛙嘴式零錢袋.jpg"},
+                new Product{ Id =6, Name = "Urbano零錢包", Category="Coinwallet", Photo="Urbano零錢包.jpg"},
+        };
+
+        //名片夾
+        List<Product> namecards = new List<Product>
+            {
+                new Product{ Id =1, Name = "Bridle 名片夾", Category="Namecard", Photo="Bridle 名片夾.jpg"},
+                new Product{ Id =2, Name = "Clarte 信封名片夾", Category="Namecard", Photo="Clarte 信封名片夾.jpg"},
+                new Product{ Id =3, Name = "Cordovan 馬臀皮名片夾", Category="Namecard", Photo="Cordovan 馬臀皮名片夾.jpg"},
+                new Product{ Id =4, Name = "Cordovan 馬臀皮票卡套", Category="Namecard", Photo="Cordovan 馬臀皮票卡套.jpg"},
+                new Product{ Id =5, Name = "Urbano 名片夾", Category="Namecard", Photo="Urbano 名片夾.jpg"},
+                new Product{ Id =6, Name = "Urbano 拉鍊卡片夾", Category="Namecard", Photo="Urbano 拉鍊卡片夾.jpg"},
+        };
+
+
+        //預設Index是顯示側背包類別
+        public ActionResult Index()
         {
-            return View();
+            return View(totebags);
         }
 
         //後背包類別
         public ActionResult Backpack()
         {
-            return View();
+            return View(backpacks);
         }
 
         //長夾類別
         public ActionResult Longclip()
         {
-            return View();
+            return View(longclips);
         }
 
         //零錢包類別
         public ActionResult Coinwallet()
         {
-            return View();
+            return View(coinwallets);
         }
 
         //名片夾類別
         public ActionResult Namecard()
         {
-            return View();
+            return View(namecards);
         }
 
-
+        //ProductDetail試做
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
 
     }
 }
