1861af3c42639f935ea960a02b9e8819c471ef53
Noelle Hsu <ohyanana2@gmail.com>
2019-11-28 16:50:14 +0800

    完善ProductController裡面的Linq，完善ProductDtail

--- Leder/Controllers/ProductController.cs
+++ Leder/Controllers/ProductController.cs
@@ -10,97 +10,94 @@ namespace Leder.Controllers
 {
     public class ProductController : Controller
     {
-        //側背包
-        List<Product> Totebags = new List<Product>
-            {      
+        
+        List<Product> products = new List<Product>
+            {
                 new Product{ Id =1, Name = "Clarte 流蘇迷你箱型包", Category="Totebag", Photo="Totebag1.jpg"},
                 new Product{ Id =2, Name = "Diario 迷你隨行斜肩袋", Category="Totebag", Photo="Totebag2.jpg"},
                 new Product{ Id =3, Name = "Plota 防水兩用斜背包", Category="Totebag", Photo="Totebag3.jpg"},
                 new Product{ Id =4, Name = "Tone Oilnume 兩用托特包", Category="Totebag", Photo="Totebag4.jpg"},
                 new Product{ Id =5, Name = "Tone Oilnume 拉鍊斜背包", Category="Totebag", Photo="Totebag5.jpg"},
                 new Product{ Id =6, Name = "Tone Oilnume 迷你郵差包", Category="Totebag", Photo="Totebag6.jpg"},
-            };
 
-        //後背包
-        List<Product> Backpacks = new List<Product>
-            {
-                new Product{ Id =1, Name = "Tone Oilnume 中型後背包", Category="Backpack", Photo="Backpack1.jpg"},
-                new Product{ Id =2, Name = "Tone Oilnume 後背包", Category="Backpack", Photo="Backpack2.jpg"},
+                new Product{ Id =7, Name = "Tone Oilnume 中型後背包", Category="Backpack", Photo="Backpack1.jpg"},
+                new Product{ Id =8, Name = "Tone Oilnume 後背包", Category="Backpack", Photo="Backpack2.jpg"},
 
-            };
+                new Product{ Id =9, Name = "Belchord 長夾", Category="Longclip", Photo="Longclip1.jpg"},
+                new Product{ Id =10, Name = "Diario 長夾", Category="Longclip", Photo="Longclip2.jpg"},
+                new Product{ Id =11, Name = "Tone Oilnume 蛙嘴式零錢長夾", Category="Longclip", Photo="Longclip3.jpg"},
+                new Product{ Id =12, Name = "Tone Oilnume 纏繞式長夾", Category="Longclip", Photo="Longclip4.jpg"},
+                new Product{ Id =13,  Name = "Urbano 長皮夾", Category="Longclip", Photo="Longclip5.jpg"},
 
-        //長夾
-        List<Product> Longclips = new List<Product>
-            {
-                new Product{ Id =1, Name = "Belchord 長夾", Category="Longclip", Photo="Longclip1.jpg"},
-                new Product{ Id =2, Name = "Diario 長夾", Category="Longclip", Photo="Longclip2.jpg"},
-                new Product{ Id =3, Name = "Tone Oilnume 蛙嘴式零錢長夾", Category="Longclip", Photo="Longclip3.jpg"},
-                new Product{ Id =4, Name = "Tone Oilnume 纏繞式長夾", Category="Longclip", Photo="Longclip4.jpg"},
-                new Product{ Id =5,  Name = "Urbano 長皮夾", Category="Longclip", Photo="Longclip5.jpg"},
+                new Product{ Id =14, Name = "Belchord零錢包", Category="Coinwallet", Photo="Coinwallet1.jpg"},
+                new Product{ Id =15, Name = "Bridle方形零錢包", Category="Coinwallet", Photo="Coinwallet2.jpg"},
+                new Product{ Id =16, Name = "Diario 雙釦式零錢包", Category="Coinwallet", Photo="Coinwallet3.jpg"},
+                new Product{ Id =17, Name = "Tone Oilnume 蛙嘴式迷你零錢包", Category="Coinwallet", Photo="Coinwallet4.jpg"},
+                new Product{ Id =18, Name = "Tone Oilnume 蛙嘴式零錢袋", Category="Coinwallet", Photo="Coinwallet5.jpg"},
+                new Product{ Id =19, Name = "Urbano零錢包", Category="Coinwallet", Photo="Coinwallet6.jpg"},
 
-        };
+                new Product{ Id =20, Name = "Bridle 名片夾", Category="Namecard", Photo="Namecard1.jpg"},
+                new Product{ Id =21, Name = "Clarte 信封名片夾", Category="Namecard", Photo="Namecard2.jpg"},
+                new Product{ Id =22, Name = "Cordovan 馬臀皮名片夾", Category="Namecard", Photo="Namecard3.jpg"},
+                new Product{ Id =23, Name = "Cordovan 馬臀皮票卡套", Category="Namecard", Photo="Namecard4.jpg"},
+                new Product{ Id =24, Name = "Urbano 名片夾", Category="Namecard", Photo="Namecard5.jpg"},
+                new Product{ Id =25, Name = "Urbano 拉鍊卡片夾", Category="Namecard", Photo="Namecard6.jpgsss"},
 
-        //零錢包
-        List<Product> Coinwallets = new List<Product>
-            {
-                new Product{ Id =1, Name = "Belchord零錢包", Category="Coinwallet", Photo="Coinwallet1.jpg"},
-                new Product{ Id =2, Name = "Bridle方形零錢包", Category="Coinwallet", Photo="Coinwallet2.jpg"},
-                new Product{ Id =3, Name = "Diario 雙釦式零錢包", Category="Coinwallet", Photo="Coinwallet3.jpg"},
-                new Product{ Id =4, Name = "Tone Oilnume 蛙嘴式迷你零錢包", Category="Coinwallet", Photo="Coinwallet4.jpg"},
-                new Product{ Id =5, Name = "Tone Oilnume 蛙嘴式零錢袋", Category="Coinwallet", Photo="Coinwallet5.jpg"},
-                new Product{ Id =6, Name = "Urbano零錢包", Category="Coinwallet", Photo="Coinwallet6.jpg"},
-        };
-
-        //名片夾
-        List<Product> Namecards = new List<Product>
-            {
-                new Product{ Id =1, Name = "Bridle 名片夾", Category="Namecard", Photo="Namecard1.jpg"},
-                new Product{ Id =2, Name = "Clarte 信封名片夾", Category="Namecard", Photo="Namecard2.jpg"},
-                new Product{ Id =3, Name = "Cordovan 馬臀皮名片夾", Category="Namecard", Photo="Namecard3.jpg"},
-                new Product{ Id =4, Name = "Cordovan 馬臀皮票卡套", Category="Namecard", Photo="Namecard4.jpg"},
-                new Product{ Id =5, Name = "Urbano 名片夾", Category="Namecard", Photo="Namecard5"},
-                new Product{ Id =6, Name = "Urbano 拉鍊卡片夾", Category="Namecard", Photo="Namecard6"},
-        };
-
-
-        //預設Index是顯示側背包類別
+    };
+        //預設是側背包
         public ActionResult Index()
         {
-            return View(Totebags);
+            List<Product> pro = new List<Product>();
+            pro  = products.Where((x) => x.Category == "Totebag").ToList();
+            return View(pro);
         }
 
-        //後背包類別
+        //後背包
         public ActionResult Backpack()
         {
-            return View(Backpacks);
+            List<Product> pro = new List<Product>();
+            pro = products.Where((x) => x.Category == "Backpack").ToList();
+            return View(pro);
+
         }
 
-        //長夾類別
+        //長夾
         public ActionResult Longclip()
         {
-            return View(Longclips);
+            List<Product> pro = new List<Product>();
+            pro = products.Where((x) => x.Category == "Longclip").ToList();
+            return View(pro);
         }
 
-        //零錢包類別
+        //零錢包
         public ActionResult Coinwallet()
         {
-            return View(Coinwallets);
+            List<Product> pro = new List<Product>();
+            pro = products.Where((x) => x.Category == "Coinwallet").ToList();
+            return View(pro);
         }
 
-        //名片夾類別
+        //名片夾
         public ActionResult Namecard()
         {
-            return View(Namecards);
+            List<Product> pro = new List<Product>();
+            pro = products.Where((x) => x.Category == "Namecard").ToList();
+            return View(pro);
         }
 
-        //ProductDetail試做
-        public ActionResult ProductDetail(int? Id, string Category) 
-        {
-            
-            var prd = cat.Where(x => x.Category == Category);
-            var item = prd.Where((x) => x.Id == Id);
-            return View(item);
 
+
+        public ActionResult ProductDetail(int? Id)
+        {
+            //var item = products.Find((x) => x.Id == Id);
+            var item = products.FirstOrDefault((x) => x.Id == Id);
+            if (item == null)  //找不到item就回傳null
+            {
+                return RedirectToAction("Index", "Home");
+              
+            }
+
+            return View(item);
         }
 
 
