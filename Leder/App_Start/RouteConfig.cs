15964438a9ed7a3eee6c73fba0db0d87f77b4a4a
淵安 證 <harry0961105819@gmail.com>
2019-11-28 14:45:56 +0800

    Checkout的路由

--- Leder/App_Start/RouteConfig.cs
+++ Leder/App_Start/RouteConfig.cs
@@ -12,11 +12,17 @@ namespace Leder
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //購物車
             routes.MapRoute(
                 name: "ShoppingCart",
                 url: "Index",
                 defaults: new { controller = "ShoppingCart", action = "Index", id = UrlParameter.Optional }
+            );
+            //結帳頁面
+            routes.MapRoute(
+                name: "Checkout",
+                url: "Checkout",
+                defaults: new { controller = "ShoppingCart", action = "Checkout", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
