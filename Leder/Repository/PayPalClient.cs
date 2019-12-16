920d30eebb51a337a3a735eddfb9e21b825e668a
淵安 證 <harry0961105819@gmail.com>
2019-12-16 11:55:38 +0800

    新增：PayPal付款功能 詳細資料： 新增(paypal.js)：接用paypal的服務器

---
+++ Leder/Repository/PayPalClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using PayPalCheckoutSdk.Core;
+using BraintreeHttp;
+
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace Samples
+{
+    public class PayPalClient
+    {
+        /**
+            Set up PayPal environment with sandbox credentials.
+            In production, use LiveEnvironment.
+         */
+        public static PayPalEnvironment environment()
+        {
+            return new SandboxEnvironment("PAYPAL-SANDBOX-CLIENT-ID","PAYPAL-SANDBOX-CLIENT-SECRET");
+        }
+
+        /**
+            Returns PayPalHttpClient instance to invoke PayPal APIs.
+         */
+        public static HttpClient client()
+        {
+            return new PayPalHttpClient(environment());
+        }
+
+        public static HttpClient client(string refreshToken)
+        {
+            return new PayPalHttpClient(environment(), refreshToken);
+        }
+
+        /**
+            Use this method to serialize Object to a JSON string.
+        */
+        public static String ObjectToJSONString(Object serializableObject)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            var writer = JsonReaderWriterFactory.CreateJsonWriter(
+                        memoryStream, Encoding.UTF8, true, true, "  ");
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(serializableObject.GetType(), new DataContractJsonSerializerSettings{UseSimpleDictionaryFormat = true});
+            ser.WriteObject(writer, serializableObject);
+            memoryStream.Position = 0;
+            StreamReader sr = new StreamReader(memoryStream);
+            return sr.ReadToEnd();
+        }
+    }
+}
---
+++ Leder/Scripts/paypal.js
@@ -0,0 +1,35 @@
+﻿        paypal.Buttons({
+            createOrder: function (data, actions) {
+                // 此功能設置交易明細，包括金額和行項目明細。
+                return actions.order.create({
+                    purchase_units: [{
+                        amount: {
+                            value: $("#TotalAmount").val()
+                        }
+                    }]
+                });
+            },
+            onApprove: function (data, actions) {
+                // 此功能從交易中獲取資金。
+                return actions.order.capture().then(function (details) {
+                    // 此功能向您的買家顯示交易成功消息。
+                    alert('您的交易已完成  金額總計：TWD ' + $("#TotalAmount").val())
+        //致電您的服務器以保存交易
+                    return fetch('/paypal-transaction-complete', {
+            method: 'post',
+                        headers: {
+                            'content-type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            orderID: data.orderID
+                        })
+                    });
+                });
+            }
+        }).render('#paypaltest');
+// 此功能會在您的網頁上顯示智能付款按鈕。
+
+
+
+
+
