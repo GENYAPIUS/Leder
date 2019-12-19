36f19516149281dfbea2acf448eb267772aaf64e
GENYA <t6589989@gmail.com>
2019-12-19 12:30:02 +0800

    將產品的Photo改成Photos

--- Leder/ViewModels/ProductViewModel.cs
+++ Leder/ViewModels/ProductViewModel.cs
@@ -13,7 +13,7 @@ namespace Leder.ViewModels
         public string Name { get; set; }    //產品名稱
         public int Price { get; set; }    //產品價格
         public string Category { get; set; }   //產品類別
-        public string Photo { get; set; }   //產品照片
+        public string Photos { get; set; }   //產品照片
         public string Description { get; set; } //產品頁面的敘述文字
 
 
