7cf8d5ebe76afcbb8d3b0b1f47ca602f1a8e8f6f
淵安 證 <harry0961105819@gmail.com>
2019-12-25 19:41:55 +0800

    修改：結帳和交易介面均不能使用商品增減功能，並顯示提醒文字 詳細： 合併shiftCartModal到shoppingModal

--- Leder/Assets/js/ShoppingCartFunction.js
+++ Leder/Assets/js/ShoppingCartFunction.js
@@ -1,4 +1,22 @@
-﻿
+﻿if (location.href == window.location.protocol + '//' + window.location.host + '/ShoppingCart/Order' || location.href == window.location.protocol + '//' + window.location.host + '/Checkout') {
+    function No() {
+        alert("再付款頁面時不能使用此功能，請在其他頁面使用");
+    }
+
+    $("#YesAddCart").click(function () {
+        No();
+    });
+    $("#NoAddCart").click(function () {
+        No();
+    });
+    function RemoveFromCart(productId) {
+        No();
+    }
+    function ClearCart() {
+        No();
+    }
+}
+else {
     //加入編號為productId的商品進購物車
 
     function RemoveFromCart(productId) {
@@ -50,5 +68,35 @@ function Overload() {
     );   
 }
 
+function ShiftCartOK() {
+    $.ajax({
+        type: 'POST',
+        url: '/ShoppingCart/ShiftCartOK',
+        data: {}
+    })
+        .done(function (msg) {
+            //將回傳的購物車頁面填入 CartLayout#Cart
+            $('#CartLayout').html(msg);
+            Overload();
+        });
+}
+
+$("#YesAddCart").click(function () {
+    $('#shiftCart').hide();
+    $('.modal-backdrop').hide();
+    for (var i = 1; i < $("#shiftCount").val(); i++) {
+        var Id = $("#" + i).val();
+        var Quantity = $("#" + (i + 1000)).val();
+        AddToCart(Id, Quantity);
+    }
+    ShiftCartOK();
+});
+
+$("#NoAddCart").click(function () {
+    $('#shiftCart').hide();
+    $('.modal-backdrop').hide();
+    ShiftCartOK();
+});
+}
 
 
