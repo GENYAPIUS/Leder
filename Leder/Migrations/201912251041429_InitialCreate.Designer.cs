2e70db575cf36d1927a12f6e9142c77eb1aa9ac1
GENYA <t6589989@gmail.com>
2019-12-25 18:44:42 +0800

    重新建Migration

--- Leder/Migrations/201912251041429_InitialCreate.Designer.cs
+++ Leder/Migrations/201912251041429_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace Leder.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class addColorDescription : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addColorDescription));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201912230922568_addColorDescription"; }
+            get { return "201912251041429_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
